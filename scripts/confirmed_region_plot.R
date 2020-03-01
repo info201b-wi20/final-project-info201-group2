b9c6726a828c3cd1bdcd391fe992a3baf8ee476a
YuchenJiao <jiaoyuchen0418@gmail.com>
2020-03-01 01:14:13 -0800

    debug summary function, finish draft of plot

---
+++ scripts/confirmed_region_plot.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+library(dplyr)
+library(ggforce)
+
+dataset <- read.csv("../data/2-27 dataset/02-27-2020.csv",
+                    stringsAsFactors = F)
+dataset <- dataset %>%
+  filter(!is.na(Confirmed))
+dataset[is.na(dataset)] <- 0
+dataset <- dataset %>%
+  group_by(Country.Region) %>%
+  summarize(total_confirmed = sum(Confirmed),
+            total_death = sum(Deaths)) %>%
+  top_n(5, wt = total_confirmed)
+confirmed_region_plot <- ggplot(dataset, aes(x = Country.Region,
+                                             y = total_confirmed,
+                                             fill = total_death)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  geom_text(aes(label = total_confirmed), vjust = 0, color = "black",
+            position = position_dodge(0.9), size = 3.5) +
+  facet_zoom(ylim = c(200, 2000)) +
+  ggtitle("Total confirmed cases vs Country/Region") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(x = "Country/Region", y = "# confirmed cases", fill = "# deaths")
--- scripts/get_summary_info.R
+++ scripts/get_summary_info.R
@@ -1,21 +1,17 @@
-# A function that takes in a dataset("../data/1-22-2020 to 2-27-2020 data/
-#                                     02-27-2020.csv")
+# A function that takes in a dataset("../data/2-27 dataset/02-27-2020.csv")
 # and returns a list of info about it:
 library(dplyr)
 get_summary_info <- function(dataset) {
-  dataset <- read.csv("../data/1-22-2020 to 2-27-2020 data/02-27-2020.csv",
-                      stringsAsFactors = F)
   dataset <- dataset %>%
     filter(!is.na(Confirmed))
   dataset[is.na(dataset)] <- 0
-  colnames(dataset)[1] <- c("Province.State")
   ret <- list()
   ret$length <- length(dataset)
   ret$country_region_affected <- length(unique(dataset$Country.Region))
   ret$province_state_affected <- length(dataset$Province.State)
-  ret$total_confirmed <- sum(Confirmed)
-  ret$total_death <- sum(Deaths)
-  ret$total_recovery <- sum(Recovered)
+  ret$total_confirmed <- sum(dataset$Confirmed)
+  ret$total_death <- sum(dataset$Deaths)
+  ret$total_recovery <- sum(dataset$Recovered)
   ret$most_affected_country_region <- dataset %>%
     filter(Confirmed == max(Confirmed)) %>%
     pull(Country.Region)
